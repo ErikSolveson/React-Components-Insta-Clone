c72ccba8fca5b9f6236618cbeb61952583184dea
Erik <erik.solveson@gmail.com>
2021-03-28 17:01:26 -0600

    finished all normal tasks

--- src/App.js
+++ src/App.js
@@ -5,20 +5,33 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React from "react";
+import { useState } from "react";
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import SearchBar from "./components/SearchBar/SearchBar";
+import Posts from "./components/Posts/Posts";
 // Import the dummyData
-import './App.css';
+import dummyData from "./dummy-data";
+import "./App.css";
 
 const App = () => {
+  const [posts, setPosts] = useState(dummyData);
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
 
-  const likePost = postId => {
+  const likePost = (postId) => {
+    const newPosts = dummyData.map((post) => {
+      if (post.id === postId) {
+        return { ...post, likes: post.likes + 1 };
+      } else {
+        return post;
+      }
+    });
+    setPosts(newPosts);
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
-
+      
       The state of the app lives at the top of the React tree, but it wouldn't be fair for nested components not to be able to change state!
       This function is passed down to nested components through props, allowing them to increase the number of likes of a given post.
 
@@ -30,9 +43,11 @@ const App = () => {
   };
 
   return (
-    <div className='App'>
+    <div className="App">
       {/* Add SearchBar and Posts here to render them */}
+      <SearchBar />
       {/* Check the implementation of each component, to see what props they require, if any! */}
+      <Posts posts={posts} likePost={likePost} />
     </div>
   );
 };
